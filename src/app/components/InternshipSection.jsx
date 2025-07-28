e7cc12c3a2f3536d4c7101a2dca63491648fc51b
Asmita <asmita.chauhan2002@gmail.com>
2025-07-29 00:23:08 +0530

    Updates

--- src/app/components/InternshipSection.jsx
+++ src/app/components/InternshipSection.jsx
@@ -42,6 +42,12 @@ const InternshipSection = () => {
   return (
     <section className='text-white'>
         <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
+            <Image 
+                className='mt-4'
+                src='/image/internship.jpg'
+                width={500} 
+                height={500}
+            />
             <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
                 <h2 className='text-4xl font-bold text-white mb-4'>Internship</h2>
                 <div className='flex flex-row mt-8'>
@@ -51,12 +57,6 @@ const InternshipSection = () => {
                 <div className='mt-8'>{TAB_DATA.find((t) => t.id === tab).content}
                 </div>
             </div>
-            <Image 
-                className='mt-4'
-                src='/image/internship.jpg'
-                width={500} 
-                height={500}
-            />
         </div>
     </section>
   );
